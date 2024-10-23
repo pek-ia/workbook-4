4088fbc89f44e77916ef47635d0d854136bf37d8
Paul Kimball <pekimball@interfaceassociates.net>
2024-10-23 11:26:13 -0400

    Added the Room class

---
+++ yearup-hotel/src/main/java/HotelApp.java
@@ -0,0 +1,12 @@
+public class HotelApp {
+    public static void main(String[] args){
+
+        boolean isClean = true;
+        boolean isAvailable = true;
+        int numberOfBeds = 2;
+
+        Room r = new Room(isClean, isAvailable, numberOfBeds);
+
+
+    }
+}
---
+++ yearup-hotel/src/main/java/Room.java
@@ -0,0 +1,13 @@
+public class Room {
+    boolean isClean;
+    boolean isAvailable;
+    int numberOfBeds;
+
+
+    public Room(boolean isClean, boolean isAvailable, int numberOfBeds){
+        this.isAvailable = isAvailable;
+        this.isClean = isClean;
+        this.numberOfBeds = numberOfBeds;
+
+    }
+}
