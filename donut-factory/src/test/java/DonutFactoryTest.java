4c48abb361bece5e0870bb0d1d984af7bd89db20
Paul Kimball <pekimball@interfaceassociates.net>
2024-10-23 16:33:28 -0400

    Better comments/naming in test class

--- donut-factory/src/test/java/DonutFactoryTest.java
+++ donut-factory/src/test/java/DonutFactoryTest.java
@@ -3,18 +3,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class DonutFactoryTest {
 
     @org.junit.jupiter.api.Test
-    void getPriceOfDonuts() {
+    void getPriceOfDonuts_halfDozen_get_10_percent_discount() {
 
-        // Arrange
-        DonutFactory df = new DonutFactory(.50);
-
+        // Fixed values for this test
         int halfDozen = 6;
         double costPerHalfDozen = 2.70;
+        double unitPricePerDonut = .50;
 
-        // Act
+        // Arrange the test object(s)
+        DonutFactory df = new DonutFactory(unitPricePerDonut);
+
+        // Act on the object(s)
         double result = df.getPriceOfDonuts(halfDozen);
 
-        // Assert
+        // Assert what SHOULD happen
         assertEquals(costPerHalfDozen, result);
 
     }
